c1da1b462d5378fe1f0bfa85fb47f8915c2bec79
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-07-02 20:53:43 -0300

    create Alert

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useEffect } from "react";
+import styles from "./Alert.module.css";
+import type { AlertType } from "@/app/types";
+
+type AlertProps = {
+  type: AlertType;
+  message: string;
+  onClose?: () => void;
+};
+
+const Alert: React.FC<AlertProps> = ({ type, message, onClose }) => {
+  useEffect(() => {
+    if (onClose) {
+      const timer = setTimeout(() => {
+        onClose();
+      }, 5000);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [onClose]);
+
+  return (
+    <div className={`${styles.alert} ${styles[type]}`} role="alert">
+      <p className={styles.message}>{message}</p>
+      {onClose && (
+        <button type="button" className={styles.closeButton} onClick={onClose}>
+          &times;
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Alert;
