664017a4ee762346e89d2e947a0269d93a7f1844
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-07-02 20:54:28 -0300

    add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,39 @@
-import Image from "next/image";
+import React from "react";
+import Header from "@/components/Header/Header";
+import About from "@/components/About/About";
+import Interests from "@/components/Interests/Interests";
+import ContactForm from "@/components/ContactForm/ContactForm";
+import Tabs, { Tab } from "@/components/Tabs/Tabs";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const userInfo = {
+    name: "Carlos Gomes",
+    imageUrl: "/path-to-image.jpg",
+    description:
+      "Soy un desarrollador web/mobile apasionado por crear experiencias digitales únicas.",
+    interests: [
+      "Desarrolo web",
+      "Desarrollo móvil",
+      "Analisis de datos",
+      "Diseño UI/UX",
+    ],
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Header name={userInfo.name} imageUrl={userInfo.imageUrl} />
+      <Tabs>
+        <Tab title="Sobre mí">
+          <About description={userInfo.description} />
+        </Tab>
+        <Tab title="Intereses">
+          <Interests interests={userInfo.interests} />
+        </Tab>
+        <Tab title="Contacto">
+          <ContactForm />
+        </Tab>
+      </Tabs>
     </main>
   );
 }
